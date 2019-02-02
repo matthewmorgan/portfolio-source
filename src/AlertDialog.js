fdac15a94b82588b38319fd31d30cab1e3e96c66
Matthew Morgan <matt@dulcetsoftware.com>
2019-02-02 10:07:33 -0500

    Commit build

---
+++ src/AlertDialog.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+export default function AlertDialog(props) {
+  return (
+      <div>
+        <Dialog
+            open={props.open}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Enjoy a Dad Joke</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {props.joke}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.handleClose} color="primary">
+              Go away
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+  )
+}
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,220 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import CameraIcon from '@material-ui/icons/PhotoCamera'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import {withStyles} from '@material-ui/core/styles'
 
-class App extends Component {
+import AlertDialog from './AlertDialog'
+
+import understanding from './img/understanding.png'
+import bread from './img/breadpos.png'
+import buzz from './img/buzzdesktop.png'
+import buzzonline from './img/buzzonline.png'
+import elephantears from './img/elephantears.png'
+import honeycreekschool from './img/honeycreekschool.png'
+import morganandyork from './img/morganandyork.png'
+import superagent from "superagent"
+
+const styles = theme => ({
+  appBar:      {
+    position: 'relative',
+  },
+  icon:        {
+    marginRight: theme.spacing.unit * 2,
+  },
+  heroUnit:    {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin:   '0 auto',
+    padding:  `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
+  },
+  layout:      {
+    width:                                                     'auto',
+    marginLeft:                                                theme.spacing.unit * 3,
+    marginRight:                                               theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width:       1100,
+      marginLeft:  'auto',
+      marginRight: 'auto',
+    },
+  },
+  cardGrid:    {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card:        {
+    height:        '100%',
+    display:       'flex',
+    flexDirection: 'column',
+  },
+  cardMedia:   {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer:      {
+    backgroundColor: theme.palette.background.paper,
+    padding:         theme.spacing.unit * 6,
+  },
+})
+
+const cards = [
+  {
+    heading:     'JSTOR Understanding Series',
+    description: 'A web application to assist high school students in researching works of literature and historical documents.  Built at Ithaka in Vue.',
+    image:       understanding,
+    href:        'https://www.jstor.org/understand'
+  },
+  {
+    heading:     'Honeycreek School',
+    description: 'A website built for a community school for social sharing of messages and photos',
+    image:       honeycreekschool
+  },
+  {
+    heading:     'POS Web App',
+    description: 'A tablet optimized point of sale system design for a fast-paced coffeehouse.  Build with Backbone on Node, Espress, and MongoDB',
+    image:       buzzonline
+  },
+  {
+    heading:     'Bread Point of Sale',
+    description: 'A touchscreen optimized retail point of sale and inventory control system.  Built in Java SE and Postgres',
+    image:       bread
+  },
+  {
+    heading:     'E-Commerce Site',
+    description: "A web app built for a children's clothing store, built with Express and integrated with their bricks and mortar POS through an API written in Express over Postgres",
+    image:       elephantears
+  },
+  {
+    heading:     'E-Commerce Site',
+    description: 'A web app built for a specialty grocery store, using Express and integrated with their bricks and mortar POS through an API written in Express over Postgres',
+    image:       morganandyork
+  },
+  {
+    heading:     'Buzz Point of Sale',
+    description: 'A touchscreen optimized retail point of sale built for a fast-paced coffee house.  Java SE and Postgres',
+    image:       buzz
+  },
+]
+
+class Album extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {...props, openDialog: false, joke: undefined}
+  }
+
+  async getDadJoke() {
+    return await superagent
+        .get('https://icanhazdadjoke.com/')
+        .set('accept', 'text/plain')
+  }
+
+  handleClick = async () => {
+    const joke = await this.getDadJoke()
+    this.setState({
+      openDialog: true,
+      joke: joke.text
+    })
+  }
+
+  handleDialogClose = () => {
+    this.setState({ openDialog: false });
+  };
+
   render() {
+    const classes = this.state.classes;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <React.Fragment>
+          <CssBaseline/>
+          <AppBar position="static" className={classes.appBar}>
+            <Toolbar>
+              <CameraIcon className={classes.icon}/>
+              <Typography variant="h6" color="inherit" noWrap>
+                Matt Morgan's Projects
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <main>
+            {/* Hero unit */}
+            <div className={classes.heroUnit}>
+              <div className={classes.heroContent}>
+                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                  Matt Morgan
+                </Typography>
+                <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                  I write software, especially modern web applications.
+                </Typography>
+                <div className={classes.heroButtons}>
+                  <Grid container spacing={16} justify="center">
+                    <Grid item>
+                      <Button variant="contained" color="primary" href="https://github.com/matthewmorgan">
+                        See my GitHub
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button variant="outlined" color="primary" onClick={this.handleClick}>
+                        Enjoy a dad joke
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </div>
+              </div>
+            </div>
+            <AlertDialog open={this.state.openDialog} joke={this.state.joke} handleClose={this.handleDialogClose}/>
+            <div className={classNames(classes.layout, classes.cardGrid)}>
+              {/* End hero unit */}
+              <Grid container spacing={40}>
+                {cards.map(card => (
+                    <Grid item key={card.description} sm={6} md={4} lg={3}>
+                      <Card className={classes.card}>
+                        <CardMedia
+                            className={classes.cardMedia}
+                            image={card.image} // eslint-disable-line max-len
+                            title="Image title"
+                        />
+                        <CardContent className={classes.cardContent}>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            {card.heading}
+                          </Typography>
+                          <Typography>
+                            {card.description}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                ))}
+              </Grid>
+            </div>
+          </main>
+          {/* Footer */}
+          <footer className={classes.footer}>
+            <Typography variant="h6" align="center" gutterBottom>
+              Thanks for checking it out!
+            </Typography>
+          </footer>
+          {/* End footer */}
+        </React.Fragment>
+    )
   }
 }
 
-export default App;
+Album.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Album)
