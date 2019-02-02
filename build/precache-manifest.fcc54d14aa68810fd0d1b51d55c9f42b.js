fae100b9a0d7c946f13498498d16fb344f27ac80
Matthew Morgan <matt@dulcetsoftware.com>
2019-02-02 10:12:02 -0500

    Add build dir

---
+++ build/precache-manifest.fcc54d14aa68810fd0d1b51d55c9f42b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "689980335ff3939ebc8b",
+    "url": "/static/css/main.562c408c.chunk.css"
+  },
+  {
+    "revision": "689980335ff3939ebc8b",
+    "url": "/static/js/main.68998033.chunk.js"
+  },
+  {
+    "revision": "271f646ef3cdd8426c54",
+    "url": "/static/js/1.271f646e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "906e22267556451585dc867d42c6dd29",
+    "url": "/static/media/understanding.906e2226.png"
+  },
+  {
+    "revision": "59aafe7b41817574d70d9d4a4058a52c",
+    "url": "/static/media/breadpos.59aafe7b.png"
+  },
+  {
+    "revision": "b8d004574bf427f10a51d47a0ddb4b05",
+    "url": "/static/media/buzzdesktop.b8d00457.png"
+  },
+  {
+    "revision": "1b51e615a107f0edb3b798b507eefd20",
+    "url": "/static/media/buzzonline.1b51e615.png"
+  },
+  {
+    "revision": "02c826c3c1ed24adf229dc18da1ab7fb",
+    "url": "/static/media/elephantears.02c826c3.png"
+  },
+  {
+    "revision": "e31572503797ff550015bc9d112effb9",
+    "url": "/static/media/honeycreekschool.e3157250.png"
+  },
+  {
+    "revision": "a4c1368e30e02795b68fa79ae9a648cf",
+    "url": "/static/media/morganandyork.a4c1368e.png"
+  },
+  {
+    "revision": "113e12b76ff7888e78bb955f1941bd62",
+    "url": "/index.html"
+  }
+];
