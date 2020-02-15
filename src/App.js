af18b3312367547f9e256322396a34a373bbf440
Matthew Morgan <matt@dulcetsoftware.com>
2020-02-15 13:26:55 -0500

    Add Khan Academy image and card

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import {withStyles} from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles'
 
 import AlertDialog from './AlertDialog'
 
@@ -23,9 +23,10 @@ import buzz from './img/buzzdesktop.png'
 import buzzonline from './img/buzzonline.png'
 import elephantears from './img/elephantears.png'
 import honeycreekschool from './img/honeycreekschool.png'
+import khanacademy from './img/khanacademy.png'
 import morganandyork from './img/morganandyork.png'
 
-import {getDadJoke} from "./dadJokeService"
+import { getDadJoke } from "./dadJokeService"
 
 
 import ReactGA from 'react-ga'
@@ -38,102 +39,108 @@ function initializeReactGA() {
 
 
 const styles = theme => ({
-  appBar:      {
+  appBar: {
     position: 'relative',
   },
-  icon:        {
+  icon: {
     marginRight: theme.spacing.unit * 2,
   },
-  heroUnit:    {
+  heroUnit: {
     backgroundColor: theme.palette.background.paper,
   },
   heroContent: {
     maxWidth: 600,
-    margin:   '0 auto',
-    padding:  `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
   },
   heroButtons: {
     marginTop: theme.spacing.unit * 4,
   },
-  layout:      {
-    width:                                                     'auto',
-    marginLeft:                                                theme.spacing.unit * 3,
-    marginRight:                                               theme.spacing.unit * 3,
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
     [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width:       1100,
-      marginLeft:  'auto',
+      width: 1100,
+      marginLeft: 'auto',
       marginRight: 'auto',
     },
   },
-  cardGrid:    {
+  cardGrid: {
     padding: `${theme.spacing.unit * 8}px 0`,
   },
-  card:        {
-    height:        '100%',
-    display:       'flex',
+  card: {
+    height: '100%',
+    display: 'flex',
     flexDirection: 'column',
   },
-  cardMedia:   {
+  cardMedia: {
     paddingTop: '56.25%', // 16:9
   },
   cardContent: {
     flexGrow: 1,
   },
-  footer:      {
+  footer: {
     backgroundColor: theme.palette.background.paper,
-    padding:         theme.spacing.unit * 6,
+    padding: theme.spacing.unit * 6,
   },
 })
 
 const cards = [
   {
-    heading:     'JSTOR Understanding Series',
+    heading: 'Khan Academy',
+    description: 'A free online k-12 education for anyone, anywhere.  React, Python, Kotlin, and Go.',
+    image: khanacademy,
+    link: 'https://www.khanacademy.org/'
+  },
+  {
+    heading: 'JSTOR Understanding Series',
     description: 'A web application to assist high school students in researching works of literature and historical documents.  Built at Ithaka in Vue.',
-    image:       understanding,
-    link:        'https://www.jstor.org/understand'
+    image: understanding,
+    link: 'https://www.jstor.org/understand'
   },
   {
-    heading:     'Honeycreek School',
+    heading: 'Honeycreek School',
     description: 'A website built for a community school for social sharing of messages and photos',
-    image:       honeycreekschool,
-    link:        'http://party.honeycreekschool.org'
+    image: honeycreekschool,
+    link: 'http://party.honeycreekschool.org'
   },
   {
-    heading:     'exercism.io',
+    heading: 'exercism.io',
     description: 'I was a maintainer and contributor to this developer education project from 2014-2019, focused primarily on the JavaScript track',
-    image:       exercism,
-    link:        'https://exercism.io'
+    image: exercism,
+    link: 'https://exercism.io'
   },
   {
-    heading:     'SEM.js',
+    heading: 'SEM.js',
     description: 'I have been on the leadership committee of this local JavaScript focused meetup for the last two years, and helped to run the mentorship study group during much of that time.',
-    image:       semjs,
-    link:        'https://www.meetup.com/SEM-JS/'
+    image: semjs,
+    link: 'https://www.meetup.com/SEM-JS/'
   },
   {
-    heading:     'POS Web App',
+    heading: 'POS Web App',
     description: 'A tablet optimized point of sale system design for a fast-paced coffeehouse.  Built with Backbone on Node, Express, and MongoDB',
-    image:       buzzonline
+    image: buzzonline
   },
   {
-    heading:     'Bread Point of Sale',
+    heading: 'Bread Point of Sale',
     description: 'A touchscreen optimized multi-client desktop retail point of sale and inventory control system.  Built in Java SE and Postgres',
-    image:       bread
+    image: bread
   },
   {
-    heading:     'E-Commerce Site',
+    heading: 'E-Commerce Site',
     description: "A web app built for a children's clothing store, built with Express and integrated with the client's bricks and mortar POS through an API written in Express over Postgres",
-    image:       elephantears
+    image: elephantears
   },
   {
-    heading:     'E-Commerce Site',
+    heading: 'E-Commerce Site',
     description: "A web app built for a specialty grocery store, using Express and integrated with the client's bricks and mortar POS through an API written in Express over Postgres",
-    image:       morganandyork
+    image: morganandyork
   },
   {
-    heading:     'Buzz Point of Sale',
+    heading: 'Buzz Point of Sale',
     description: 'A touchscreen optimized retail point of sale built for a fast-paced coffee house.  Java SE and Postgres',
-    image:       buzz
+    image: buzz
   },
 ]
 
@@ -141,7 +148,7 @@ initializeReactGA()
 
 
 const trackDialogEvent = (open) => {
-  if(open){
+  if (open) {
     ReactGA.event({
       category: 'Engagement',
       action: 'Close Joke Dialog'
@@ -163,17 +170,17 @@ const trackLink = (link) => {
 }
 
 function Album(props) {
-  const {classes} = {...props}
+  const { classes } = { ...props }
   const noUnderline = {
     textDecoration: 'none'
   }
 
-  const [ dialogState, setDialogState ] = useState({open: false, joke: ''});
+  const [dialogState, setDialogState] = useState({ open: false, joke: '' });
 
   const changeDialogState = async () => {
     trackDialogEvent(dialogState.open)
-    if (dialogState.open){
-      setDialogState({open: false})
+    if (dialogState.open) {
+      setDialogState({ open: false })
     } else {
       const joke = await getDadJoke()
       setDialogState({
@@ -185,85 +192,85 @@ function Album(props) {
 
 
   return (
-      <React.Fragment>
-        <CssBaseline/>
-        <AppBar position="static" className={classes.appBar}>
-          <Toolbar>
-            <CameraIcon className={classes.icon}/>
-            <Typography variant="h6" color="inherit" noWrap>
-              Matt Morgan's Projects
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Matt Morgan's Projects
             </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          {/* Hero unit */}
-          <div className={classes.heroUnit}>
-            <div className={classes.heroContent}>
-              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Matt Morgan
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Matt Morgan
               </Typography>
-              <Typography variant="h6" align="center" color="textSecondary" paragraph>
-                I write software, especially modern web applications.
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              I write software, especially modern web applications.
               </Typography>
-              <div className={classes.heroButtons}>
-                <Grid container spacing={16} justify="center">
-                  <Grid item>
-                    <Button variant="contained" color="primary" href="https://github.com/matthewmorgan"
-                            rel="noopener noreferrer" target="_blank" onClick={() => {
+            <div className={classes.heroButtons}>
+              <Grid container spacing={16} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary" href="https://github.com/matthewmorgan"
+                    rel="noopener noreferrer" target="_blank" onClick={() => {
                       trackLink("https://github.com/matthewmorgan")
                     }}>
-                      See my GitHub
+                    See my GitHub
                     </Button>
-                  </Grid>
-                  <Grid item>
-                    <Button variant="outlined" color="primary" onClick={changeDialogState}>
-                      Enjoy a dad joke
-                    </Button>
-                  </Grid>
                 </Grid>
-              </div>
+                <Grid item>
+                  <Button variant="outlined" color="primary" onClick={changeDialogState}>
+                    Enjoy a dad joke
+                    </Button>
+                </Grid>
+              </Grid>
             </div>
           </div>
-          <AlertDialog {...dialogState} handleClose={changeDialogState}/>
-          <div className={classNames(classes.layout, classes.cardGrid)}>
-            {/* End hero unit */}
-            <Grid container spacing={40}>
-              {cards.map(card => (
-                  <Grid item key={card.description} sm={6} md={4} lg={3}>
-                    <Card className={classes.card}>
-                      <CardMedia
-                          className={classes.cardMedia}
-                          image={card.image} // eslint-disable-line max-len
-                          title={card.heading}
-                      />
-                      <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {card.link &&
-                          <a href={card.link} style={noUnderline} target="_blank" rel="noopener noreferrer"
-                             onClick={() => {trackLink(card.link)}}>{card.heading}</a>}
-                          {!card.link && card.heading}
-                        </Typography>
-                        <Typography>
-                          {card.description}
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-              ))}
-            </Grid>
-          </div>
-        </main>
-        {/* Footer */}
-        <footer className={classes.footer}>
-          <Typography variant="h6" align="center" gutterBottom>
-            Thanks for checking it out!
+        </div>
+        <AlertDialog {...dialogState} handleClose={changeDialogState} />
+        <div className={classNames(classes.layout, classes.cardGrid)}>
+          {/* End hero unit */}
+          <Grid container spacing={40}>
+            {cards.map(card => (
+              <Grid item key={card.description} sm={6} md={4} lg={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={card.image} // eslint-disable-line max-len
+                    title={card.heading}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.link &&
+                        <a href={card.link} style={noUnderline} target="_blank" rel="noopener noreferrer"
+                          onClick={() => { trackLink(card.link) }}>{card.heading}</a>}
+                      {!card.link && card.heading}
+                    </Typography>
+                    <Typography>
+                      {card.description}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Thanks for checking it out!
           </Typography>
-          <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-            Built in React using Material Design components and styles
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Built in React using Material Design components and styles
           </Typography>
-        </footer>
-        {/* End footer */}
-      </React.Fragment>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
   )
 }
 
